539e64d3dd46947eeea53d3d9a4a80871c6ee48c
ritzier <ritzierwheat@gmail.com>
2024-07-29 22:58:47 +0800

    Modified

--- update_mirrorlist.sh
+++ update_mirrorlist.sh
@@ -1,1 +1,1 @@
-curl -s "https://archlinux.org/mirrorlist/?country=SG&protocol=https&ip_version=4" | sed 's/^#Server/Server/' > mirrorlist.txt
+curl -s "https://archlinux.org/mirrorlist/?country=SG&protocol=https&ip_version=4" | sed 's/^#Server/Server/' > /etc/pacman.d/mirrorlist
