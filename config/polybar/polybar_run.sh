5def879d28e770deb1018112cdb2c1117be9d586
ritzier <ritzierwheat@gmail.com>
2024-09-18 09:02:42 +0800

    feat: polybar start script

--- config/polybar/polybar_run.sh
+++ config/polybar/polybar_run.sh
@@ -7,7 +7,7 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar1 and bar2
-polybar bar &
+polybar top
 # polybar external_bottom &
 
 echo "Bars launched..."
